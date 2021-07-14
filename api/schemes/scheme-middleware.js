f55c6414f2381b304d16f362aace0703f11141bb
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-14 13:46:06 -0700

    create validateScheme middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -32,7 +32,21 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {};
+const validateScheme = (req, res, next) => {
+  try {
+    const newScheme = !req.body.scheme_name;
+    if (!newScheme || newScheme === "" || typeof newScheme !== "string") {
+      next({
+        status: 400,
+        message: "invalid scheme_name",
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
