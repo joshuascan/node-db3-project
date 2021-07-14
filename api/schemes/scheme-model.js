f5c10e0d8e4ce8f3da07b43b16aaa0cafcea43d0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-14 15:50:44 -0700

    create findById model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -20,25 +20,35 @@ function find() {
   */
   return db("schemes as sc")
     .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
-    .count("st.step_id as number_of_steps")
     .select("sc.*")
-    .groupBy("sc.scheme_id")
-    .orderBy("sc.scheme_id", "asc");
+    .count("st.step_id as number_of_steps")
+    .groupBy("sc.scheme_id");
 }
 
-function findById(scheme_id) {
-  const scheme = db("schemes as sc")
+async function findById(scheme_id) {
+  const rows = await db("schemes as sc")
     .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
-    .select(
-      "sc.scheme_id",
-      "sc.scheme_name",
-      "st.step_id",
-      "st.step_number",
-      "st.instructions"
-    )
+    .select("sc.scheme_name", "st.*", "sc.scheme_id")
     .where("sc.scheme_id", scheme_id)
     .orderBy("st.step_number", "asc");
-  return scheme;
+
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps: [],
+  };
+
+  rows.forEach((row) => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions,
+      });
+    }
+  });
+
+  return result;
   // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
